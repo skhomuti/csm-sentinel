c02fcb68152d5900d167f41c2bad3f45356f099f
skhomuti <skhomuti@gmail.com>
2025-07-23 17:24:31 +0500

    feat: add CSM v2 compatibility

--- src/csm_bot/models.py
+++ src/csm_bot/models.py
@@ -2,12 +2,20 @@ import dataclasses
 import json
 import os
 
+from eth_typing import ChecksumAddress
 from hexbytes import HexBytes
 
 
 CSM_ABI = json.load(open("abi/CSModule.json"))
+CSM_V2_ABI = json.load(open("abi/CSModuleV2.json"))
+
 ACCOUNTING_ABI = json.load(open("abi/CSAccounting.json"))
+ACCOUNTING_V2_ABI = json.load(open("abi/CSAccountingV2.json"))
+
 FEE_DISTRIBUTOR_ABI = json.load(open("abi/CSFeeDistributor.json"))
+FEE_DISTRIBUTOR_V2_ABI = json.load(open("abi/CSFeeDistributorV2.json"))
+
+PARAMETERS_REGISTRY_ABI = json.load(open("abi/CSParametersRegistry.json"))
 VEBO_ABI = json.load(open("abi/VEBO.json"))
 
 ETHERSCAN_BLOCK_URL_TEMPLATE = os.getenv("ETHERSCAN_URL") + "/block/{}"
@@ -26,6 +34,7 @@ class Event:
     args: dict
     block: int
     tx: HexBytes
+    address: ChecksumAddress
 
     def readable(self):
         args = ", ".join(f"{key}={value}" for key, value in self.args.items())
